4094f01986d0eaa7ed40f0c6dbe57ef2f049f1f3
Mary Griffus <mary.e.shultz@gmail.com>
2016-10-22 07:10:07 -0400

    Modify the commented instructions

--- geohash.py
+++ geohash.py
@@ -4,11 +4,11 @@ BASE_32 = "0123456789bcdefghjkmnpqrstuvwxyz"
 def geohash(lat, lon):
     lat_range = [-90, 90]
     lon_range = [-180, 180]
-    # find column for lat and 'label' it with binary
+    # find column for lon and 'label' it with binary
 
-    # find row for lon and 'label' it with binary
+    # find row for lat and 'label' it with binary
 
-    # interleave (alternate) the lat and lon bits
+    # interleave (alternate) the lon and lat bits
 
     # encode the resulting number in base 32 (easier to use than a big long binary string!)
 
